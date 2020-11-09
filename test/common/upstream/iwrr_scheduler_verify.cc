58b6880f3da0808ac66cfb48e9213c3e90b6abc1
Tony Allen <tony@allen.gg>
2020-11-08 18:31:07 -0800

    more
    Signed-off-by: Tony Allen <tony@allen.gg>

--- test/common/upstream/iwrr_scheduler_verify.cc
+++ test/common/upstream/iwrr_scheduler_verify.cc
@@ -7,9 +7,11 @@
 
 using namespace Envoy::Upstream;
 
-void syncCheckIWRR() {
-  IWRRScheduler<uint32_t> edf1, edf2;
+/**
+ * Hacking together some quick checks.
+ */
 
+size_t syncCheck(Scheduler<uint32_t>& sched1, Scheduler<uint32_t>& sched2) {
   size_t collisions = 0;
 
   std::vector<std::pair<double, std::shared_ptr<uint32_t>>> inputs;
@@ -18,72 +20,96 @@ void syncCheckIWRR() {
   }
 
   std::random_shuffle(inputs.begin(), inputs.end());
+
   for (const auto& p : inputs) {
-    edf1.add(p.first, p.second);
+    sched1.add(p.first, p.second);
   }
 
   std::random_shuffle(inputs.begin(), inputs.end());
+
   for (const auto& p : inputs) {
-    edf2.add(p.first, p.second);
+    sched2.add(p.first, p.second);
   }
 
+  auto fn = [](const auto& obj) { return static_cast<double>(obj); };
   for (int i = 0; i < 10000; ++i) {
-    auto r1 = edf1.pickAndAdd([](const auto& obj) {
-      return static_cast<double>(obj);
-    });
-    auto r2 = edf2.pickAndAdd([](const auto& obj) {
-      return static_cast<double>(obj);
-    });
-
+    auto r1 = sched1.pickAndAdd(fn);
+    auto r2 = sched2.pickAndAdd(fn);
     if (*r1 == *r2) {
       ++collisions;
     }
   }
 
-  std::cout << "IWRR collisions: " << collisions << std::endl;
+  return collisions;
+}
+
+void selectionCounts(Scheduler<uint32_t>& sched) {
+  std::cout << "diff between observed and expected % (1.0 means 1%)\n";
+
+  // Try 16 objects for sane plots.
+  constexpr size_t num_objs = 16;
+
+  constexpr size_t n = 10000;
+
+  std::vector<std::pair<double, std::shared_ptr<uint32_t>>> inputs;
+  for (uint32_t i = 1; i <= num_objs; ++i) {
+    inputs.emplace_back(static_cast<double>(i), std::make_shared<uint32_t>(i));
+  }
+
+  std::random_shuffle(inputs.begin(), inputs.end());
+  for (const auto& p : inputs) {
+    sched.add(p.first, p.second);
+  }
+
+
+  std::unordered_map<uint32_t, size_t> selection_counts;
+  auto fn = [](const auto& obj) { return static_cast<double>(obj); };
+  for (size_t i = 0; i < n; ++i) {
+    auto p = sched.pickAndAdd(fn);
+    selection_counts[*p]++;
+  }
+
+  double weight_sum = 0;
+  for (const auto& i : inputs) {
+    weight_sum += i.first;
+  }
+
+  for (const auto& p : selection_counts) {
+    const double observed_pct = 100 * static_cast<double>(p.second) / n;
+    const double expected_pct = 100 * static_cast<double>(p.first) / weight_sum;
+    std::cout << (expected_pct - observed_pct) << std::endl;
+  }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void syncCheckIWRR() {
+  IWRRScheduler<uint32_t> iwrr1, iwrr2;
+  std::cout << "IWRR collisions: " << syncCheck(iwrr1, iwrr2) << std::endl;
 }
 
 void syncCheckEDF() {
   EdfScheduler<uint32_t> edf1, edf2;
+  std::cout << "EDF collisions: " << syncCheck(edf1, edf2) << std::endl;
+}
 
-  size_t collisions = 0;
+void countsIWRR() {
+  std::cout << "---\nIWRR selections\n--\n";
+  IWRRScheduler<uint32_t> iwrr;
+  selectionCounts(iwrr);
+}
 
-  std::vector<std::pair<double, std::shared_ptr<uint32_t>>> inputs;
-  for (uint32_t i = 1; i < 1000; ++i) {
-    inputs.emplace_back(i, std::make_shared<uint32_t>(i));
-  }
-
-  std::random_shuffle(inputs.begin(), inputs.end());
-  for (const auto& p : inputs) {
-    edf1.add(p.first, p.second);
-  }
-
-  std::random_shuffle(inputs.begin(), inputs.end());
-  for (const auto& p : inputs) {
-    edf2.add(p.first, p.second);
-  }
-
-  for (int i = 0; i < 10000; ++i) {
-    auto r1 = edf1.pickAndAdd([](const auto& obj) {
-      return static_cast<double>(obj);
-    });
-    auto r2 = edf2.pickAndAdd([](const auto& obj) {
-      return static_cast<double>(obj);
-    });
-
-    if (*r1 == *r2) {
-      ++collisions;
-    }
-  }
-
-  std::cout << "EDF collisions: " << collisions << std::endl;
+void countsEDF() {
+  std::cout << "---\nEDF selections\n--\n";
+  EdfScheduler<uint32_t> edf;
+  selectionCounts(edf);
 }
 
 int main() {
-  EdfScheduler<uint32_t> edf;
-
   syncCheckEDF();
   syncCheckIWRR();
+  countsEDF();
+  countsIWRR();
 
   return 0;
 }
